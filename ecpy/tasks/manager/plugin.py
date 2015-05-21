93b3106020cd72a726530f1f4b3384962f9ecb7b
Matthieu Dartiailh <marul@laposte.net>
2015-05-21 16:24:06 +0200

    Add rough manager structure.

---
+++ ecpy/tasks/manager/plugin.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2015 by Ecpy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""
+
+"""
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
+
+from ...utils.plugin_tools import HasPrefPlugin
+
+
+class TaskPlugin(HasPrefPlugin):
+    """
+    """
+
+    def task_exists(self, task):
+        """
+        """
+        pass
+
+    def interface_exists(self, interface):
+        """
+        """
+        pass
