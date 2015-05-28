3b9685ae90ecc8e6a4f12e35ba54aa78d280d7fc
mdartiailh <m.dartiailh@gmail.com>
2015-05-29 00:44:55 +0200

    Add a watchdog utility module for common file observer. remove uneeded chain_map.

---
+++ ecpy/utils/watchdog.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2015 by Ecpy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""Collections of useful watchdog file system observers.
+
+"""
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
+from watchdog.events import (FileSystemEventHandler, FileCreatedEvent,
+                             FileDeletedEvent, FileMovedEvent)
+
+
+class SystematicFileUpdater(FileSystemEventHandler):
+    """Simple watchdog handler calling always the same function no matter the
+    event
+
+    """
+    def __init__(self, handler):
+        self.handler = handler
+
+    def on_created(self, event):
+        super(SystematicFileUpdater, self).on_created(event)
+        if isinstance(event, FileCreatedEvent):
+            self.handler()
+
+    def on_deleted(self, event):
+        super(SystematicFileUpdater, self).on_deleted(event)
+        if isinstance(event, FileDeletedEvent):
+            self.handler()
+
+    def on_moved(self, event):
+        super(SystematicFileUpdater, self).on_deleted(event)
+        if isinstance(event, FileMovedEvent):
+            self.handler()
