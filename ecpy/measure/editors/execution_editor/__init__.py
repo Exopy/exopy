c698bf89d31e570dd3526cd61fff5cc5f08acb22
MatthieuDartiailh <m.dartiailh@gmail.com>
2015-08-21 11:08:59 +0200

    Finish execution editor.

--- ecpy/measure/editors/execution_editor/__init__.py
+++ ecpy/measure/editors/execution_editor/__init__.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2015 by Ecpy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""Editor allowing to customize the execution parameters of a task.
+"""
 import enaml
 with enaml.imports():
-    from .editor_manifest import  ExecutionEditorManifest
+    from .editor import ExecutionEditor
+
+__all__ = ['ExecutionEditor']
