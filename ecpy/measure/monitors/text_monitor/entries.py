bd02fcf781d324f4cd773e9cc199d1edb9e37b9a
MatthieuDartiailh <m.dartiailh@gmail.com>
2015-09-13 23:00:02 +0200

    WIP cleaning up the TextMonitor.

--- ecpy/measure/monitors/text_monitor/entries.py
+++ ecpy/measure/monitors/text_monitor/entries.py
@@ -1,31 +1,39 @@
 # -*- coding: utf-8 -*-
-# =============================================================================
-# module : entries.py
-# author : Matthieu Dartiailh
-# license : MIT license
-# =============================================================================
+# -----------------------------------------------------------------------------
+# Copyright 2015 by Ecpy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""Entries that can be displayed by the text monitor.
+
+"""
+from __future__ import (division, unicode_literals, print_function,
+                        absolute_import)
+
 from atom.api import (Unicode, List)
 from enaml.application import deferred_call
 from hqc_meas.utils.atom_util import HasPrefAtom
 
 
 class MonitoredEntry(HasPrefAtom):
-    """ Entry to display by the text monitor.
+    """Entry to display by the text monitor.
 
     """
-    # User understandable name of the monitor entry.
+    #: User understandable name of the monitor entry.
     name = Unicode().tag(pref=True)
 
-    # Full name of the entry as found or built from the database.
+    #: Full name of the entry as found or built from the database.
     path = Unicode().tag(pref=True)
 
-    # Formatting of the entry.
+    #: Formatting of the entry.
     formatting = Unicode().tag(pref=True)
 
-    # Current value that the monitor should display.
+    #: Current value that the monitor should display.
     value = Unicode()
 
-    # List of database entries the entry depend_on.
+    #: List of database entries the entry depend_on.
     depend_on = List().tag(pref=True)
 
     def update(self, database_vals):
@@ -33,7 +41,7 @@ class MonitoredEntry(HasPrefAtom):
         the database.
 
         """
-        # TODO :  handle evaluation delimited by $
+        # TODO :  handle evaluation delimited by $. Imply a try except
         vals = {d: database_vals[d] for d in self.depend_on}
         new_val = self.formatting.format(**vals)
         deferred_call(setattr, self, 'value', new_val)
--- ecpy/measure/monitors/text_monitor/monitor.py
+++ ecpy/measure/monitors/text_monitor/monitor.py
@@ -13,15 +13,17 @@
 from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
 
+from ast import literal_eval
+from textwrap import fill
+
 import enaml
-from atom.api import (List, Dict, ForwardTyped, Typed)
+from atom.api import (List, Dict, ForwardTyped)
 
 from ..base_monitor import BaseMonitor
 from .entries import MonitoredEntry
 
 with enaml.imports():
     from enaml.stdlib.message_box import information
-    from .monitor_views import (TextMonitorView)
 
 
 def import_monitor_plugin():
@@ -32,7 +34,6 @@ def import_monitor_plugin():
     return TextMonitorPlugin
 
 
-# XXXX
 class TextMonitor(BaseMonitor):
     """Simple monitor displaying entries as text in a widget.
 
@@ -59,125 +60,117 @@ class TextMonitor(BaseMonitor):
     #: List of user created monitor entries.
     custom_entries = List(MonitoredEntry)
 
-    # XXXX
-    def start(self, parent_ui):
-        """
-        """
-        if self.auto_show:
-            self.show_monitor(parent_ui)
-
-    # XXXX
-    def stop(self):
-        """
-        """
-        # Avoid raising errors if the view has already been destroyed.
-        if getattr(self._view, 'proxy_is_active', None):
-            self._view.close()
-        self._view = None
-
-    # XXXX
     def process_news(self, news):
+        """Handle a news by calling every related entrt updater.
+
         """
-        """
+        key, value = news
         values = self._database_values
-        values[news[0]] = news[1]
-        for updater in self.updaters[news[0]]:
+        values[key] = value
+        for updater in self.updaters[key]:
             updater(values)
 
-    # XXXX
-    def refresh_monitored_entries(self, entries={}):
-        """
+    def refresh_monitored_entries(self, entries=None):
+        """Rebuild entries based on the rules and database entries.
+
+        Parameters
+        ----------
+        entries : dict, optional
+            Database entries to use when rebuilding the monitor entries.
+
         """
         if not entries:
             entries = self._database_values
         else:
             self._database_values = entries
 
+        # Preserve the custom entries.
         custom = self.custom_entries[:]
-        self.clear_state()
+
+        self._clear_state()
         self.custom_entries = custom
+
         for entry, value in entries.iteritems():
-            self.database_modified({'value': (entry, value)})
+            self.handle_database_change(('added', entry, value))
 
     # XXXX
     def handle_database_change(self, news):
-        entry = change['value']
+        """Generate new entries for added values and clean removed values.
 
+        """
         # Handle the addition of a new entry to the database
-        if len(entry) > 1:
+        if news[0] == 'added':
+
+            _, path, value = news
 
             # Store the new value.
-            self._database_values[entry[0]] = entry[1]
+            self._database_values[path] = value
 
             # Add a default entry to the displayed monitor entries.
-            self.displayed_entries.append(self._create_default_entry(*entry))
+            new_entry = self._create_default_entry(path, value)
+            self.displayed_entries.append(new_entry)  # XXXX GUI
 
             # Try to apply rules.
             for rule in self.rules:
-                rule.try_apply(entry[0], self)
+                rule.try_apply(path, self)
 
             # Check whether any custom entry is currently hidden.
             hidden_custom = [e for e in self.custom_entries
-                             if e not in self.displayed_entries
-                             or e not in self.undisplayed_entries]
+                             if e not in self.displayed_entries or
+                             e not in self.undisplayed_entries]
 
             # If there is one checks whether all the dependences are once
             # more available.
             if hidden_custom:
                 for e in hidden_custom:
                     if all(d in self.database_entries for d in e.depend_on):
-                        self.displayed_entries.append(e)
+                        self.displayed_entries.append(e)  # XXXX GUI
 
         # Handle the case of a database entry being suppressed, by removing all
         # monitors entries which where depending on this entry.
-        else:
-            self.displayed_entries[:] = [m for m in self.displayed_entries
-                                         if entry[0] not in m.depend_on]
-            self.undisplayed_entries[:] = [m for m in self.undisplayed_entries
-                                           if entry[0] not in m.depend_on]
-            self.hidden_entries[:] = [m for m in self.hidden_entries
-                                      if entry[0] not in m.depend_on]
+        elif news[0] == 'removed':
 
-            if entry[0] in self.database_entries:
-                self.database_entries.remove(entry[0])
+            _, path = news
+            self.displayed_entries = [m for m in self.displayed_entries
+                                      if path not in m.depend_on]
+            self.undisplayed_entries = [m for m in self.undisplayed_entries
+                                        if path not in m.depend_on]
+            self.hidden_entries = [m for m in self.hidden_entries
+                                   if path not in m.depend_on]
 
-            if entry[0] in self._database_values:
-                del self._database_values[entry[0]]
+            if path in self.database_entries:
+                self.database_entries.remove(path)
 
-    # XXXX
-    def clear_state(self):
-        """ Clear the monitor state.
+            if path in self._database_values:
+                del self._database_values[path]
+
+    def get_state(self):
+        """Write the state of the monitor in a dictionary.
 
         """
-        with self.suppress_notifications():
-            self.displayed_entries = []
-            self.undisplayed_entries = []
-            self.hidden_entries = []
-            self.updaters = {}
-            self.custom_entries = []
-            self.database_entries = []
-
-    # XXXX
-    def get_state(self):
         prefs = self.preferences_from_members()
-        # Save the definitions of the custom entries.
+
+        # Get the definitions of the custom entries.
         for i, custom_entry in enumerate(self.custom_entries):
             aux = 'custom_{}'.format(i)
             prefs[aux] = custom_entry.preferences_from_members()
 
-        # Save the definitions of the rules.
+        # Get the definitions of the rules.
         for i, rule in enumerate(self.rules):
             aux = 'rule_{}'.format(i)
             prefs[aux] = rule.preferences_from_members()
 
+        # Get the state of each entry based on its path.
         prefs['displayed'] = repr([e.path for e in self.displayed_entries])
         prefs['undisplayed'] = repr([e.path for e in self.undisplayed_entries])
         prefs['hidden'] = repr([e.path for e in self.hidden_entries])
 
         return prefs
 
-    # XXXX
     def set_state(self, config, entries):
+        """Rebuild all rules and dispatch entries according to the state.
+
+        """
         # Request all the rules class from the plugin.
         rules_config = [conf for name, conf in config.iteritems()
                         if name.startswith('rule_')]
@@ -203,20 +196,21 @@ class TextMonitor(BaseMonitor):
         m_entries = set(self.displayed_entries + self.undisplayed_entries +
                         self.hidden_entries + self.custom_entries)
 
-        pref_disp = eval(config['displayed'])
-        pref_undisp = eval(config['undisplayed'])
-        pref_hidden = eval(config['hidden'])
+        pref_disp = literal_eval(config['displayed'])
+        pref_undisp = literal_eval(config['undisplayed'])
+        pref_hidden = literal_eval(config['hidden'])
+
         disp = [e for e in m_entries if e.path in pref_disp]
         m_entries -= set(disp)
         undisp = [e for e in m_entries if e.path in pref_undisp]
         m_entries -= set(undisp)
         hidden = [e for e in m_entries if e.path in pref_hidden]
         m_entries -= set(hidden)
+
         if m_entries:
             e_l = [e.name for e in m_entries]
-            mess = cleandoc('''The following entries were not
-                        expected from the config : {} . These entries has been
-                        added to the displayed ones.''')
+            mess = ('The following entries were not expected from the config :'
+                    ' {}. These entries has been added to the displayed ones.')
             information(parent=None,
                         title='Unhandled entries',
                         text=fill(mess.format(e_l)))
@@ -225,18 +219,6 @@ class TextMonitor(BaseMonitor):
         self.displayed_entries = disp
         self.undisplayed_entries = undisp
         self.hidden_entries = hidden
-        self.measure_name = config['measure_name']
-        self.auto_show = eval(config['auto_show'])
-
-    # XXXX
-    def show_monitor(self, parent_ui):
-        if self._view and self._view.proxy_is_active:
-            self._view.restore()
-            self._view.send_to_front()
-        else:
-            view = TextMonitorView(monitor=self)
-            view.show()
-            self._view = view
 
     # XXXX
     @property
@@ -246,7 +228,6 @@ class TextMonitor(BaseMonitor):
         """
         return self._database_values.keys()
 
-    # XXXX
     def add_rule_to_plugin(self, rule_name):
         """ Add a rule definition to the plugin.
 
@@ -254,6 +235,7 @@ class TextMonitor(BaseMonitor):
         ----------
         rule_name : unicode
             Name of the rule whose description should be added to the plugin.
+            If the rule is already known (same name) this is a no-op.
 
         """
         plugin = self._plugin
@@ -282,17 +264,13 @@ class TextMonitor(BaseMonitor):
     #: Reference to the monitor plugin handling the rules persistence.
     _plugin = ForwardTyped(import_monitor_plugin)
 
-    #: Reference to the current display
-    _view = Typed(TextMonitorView)
-
-    # XXXX
     @staticmethod
     def _create_default_entry(entry_path, value):
         """ Create a monitor entry for a database entry.
 
         Parameters
         ----------
-        entry_path : str
+        entry_path : unicode
             Path of the database entries for which to create a monitor entry.
 
         Returns
@@ -301,12 +279,24 @@ class TextMonitor(BaseMonitor):
             Monitor entry to be added to the monitor.
 
         """
-        name = entry_path.rsplit('/', 1)[-1]
+        _, name = entry_path.rsplit('/', 1)
         formatting = '{' + entry_path + '}'
         entry = MonitoredEntry(name=name, path=entry_path,
                                formatting=formatting, depend_on=[entry_path])
         entry.value = '{}'.format(value)
         return entry
+
+    def _clear_state(self):
+        """ Clear the monitor state.
+
+        """
+        with self.suppress_notifications():  # Need to clarify this
+            self.displayed_entries = []
+            self.undisplayed_entries = []
+            self.hidden_entries = []
+            self.updaters = {}
+            self.custom_entries = []
+            self.database_entries = []
 
     # XXXX
     def _observe_displayed_entries(self, change):
