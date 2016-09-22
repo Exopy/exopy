dffb2b556a1281264c625f3c67da288a2d5405ec
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-09-22 16:16:52 +0200

    update version and CHANGES

--- ecpy/version.py
+++ ecpy/version.py
@@ -28,7 +28,7 @@ MINOR = 1
 MICRO = 0
 
 # The status indicate if this is a development or pre-release version
-STATUS = 'b1'
+STATUS = 'b2'
 
 #: A namedtuple of the version info for the current release.
 version_info = namedtuple('version_info', 'major minor micro status')
