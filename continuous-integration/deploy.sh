8c124b2cfef1961591f99f8917e8498cd075b73f
MatthieuDartiailh <marul@laposte.net>
2017-03-09 16:29:27 +0100

    travis: use deploy step for Anaconda upload

---
+++ continuous-integration/deploy.sh
@@ -0,0 +1,7 @@
+set -e
+PACK="$(conda build conda --output)"
+conda convert $PACK -p osx-64 -p win-64 --quiet -o $CONDA_BLD_PATH
+conda convert $PACK -p win-32 --quiet -o $CONDA_BLD_PATH
+cd $CONDA_BLD_PATH
+source deactivate
+python $TRAVIS_BUILD_DIR/continuous-integration/anaconda-push.py
