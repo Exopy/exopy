2f104648e905640a60fec3acf3238de64cbd05c0
MatthieuDartiailh <marul@laposte.net>
2017-03-09 20:01:49 +0100

    travis try using TRAVIS_PULL_REQUEST
    This will allow to debug the script at least hopefully

--- continuous-integration/deploy.sh
+++ continuous-integration/deploy.sh
@@ -1,4 +1,5 @@
 set -e
+cd $TRAVIS_BUILD_DIR
 PACK="$(conda build conda --output)"
 conda convert $PACK -p osx-64 -p win-64 --quiet -o $CONDA_BLD_PATH
 conda convert $PACK -p win-32 --quiet -o $CONDA_BLD_PATH
