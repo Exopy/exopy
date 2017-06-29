ac4b85b90c23c0c60e6968fb572c55f3c9ab5709
MatthieuDartiailh <marul@laposte.net>
2017-06-29 16:16:43 +0200

    release: prepare 0.1.0 release

--- ecpy/version.py
+++ ecpy/version.py
@@ -28,7 +28,7 @@ MINOR = 1
 MICRO = 0
 
 # The status indicate if this is a development or pre-release version
-STATUS = 'b4'
+STATUS = ''
 
 #: A namedtuple of the version info for the current release.
 version_info = namedtuple('version_info', 'major minor micro status')
--- setup.py
+++ setup.py
@@ -9,6 +9,16 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 from ecpy.version import __version__
 
+
+def enaml_build_py(*args, **kwargs):
+    from enaml.build_tools import EnamlBuildPy
+    return EnamlBuildPy(*args, **kwargs)
+
+
+def enaml_install_lib(*args, **kwargs):
+    from enaml.build_tools import EnamlInstallLib
+    return EnamlInstallLib(*args, **kwargs)
+
 setup(
     name='ecpy',
     description='Experiment control application',
@@ -32,10 +42,13 @@ setup(
         ],
     zip_safe=False,
     packages=find_packages(exclude=['tests', 'tests.*']),
-    package_data={'': ['*.enaml', '*.txt']},
-    requires=['future', 'pyqt', 'atom', 'enaml', 'kiwisolver', 'configobj',
-              'watchdog', 'setuptools', 'qtawesome'],
-    install_requires=['setuptools', 'future', 'atom', 'enaml', 'kiwisolver',
-                      'configobj', 'watchdog', 'qtawesome'],
-    entry_points={'gui_scripts': 'ecpy = ecpy.__main__:main'}
+    package_data={'': ['*.txt']},
+    requires=['future', 'pyqt', 'atom', 'enaml', 'kiwisolver',
+              'configobj', 'watchdog', 'setuptools', 'qtawesome'],
+    setup_requires=['setuptools', 'enaml >= 0.10.2.dev'],
+    install_requires=['setuptools', 'future', 'atom', 'enaml >= 0.10.2.dev',
+                      'kiwisolver', 'configobj', 'watchdog', 'qtawesome'],
+    entry_points={'gui_scripts': 'ecpy = ecpy.__main__:main'},
+    cmdclass={'build_py': enaml_build_py,
+              'install_lib': enaml_install_lib}
 )
