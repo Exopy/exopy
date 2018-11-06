4e06c4ab050eb81b8176850ba959830e07bd97e7
Matthieu Dartiailh <marul@laposte.net>
2018-11-06 10:48:29 -0500

    setup: add PyQt5 to install requires only if PyQt is absent (#155)
    * setup: add PyQt5 to install requires only if PyQt is absent
    
    This is a kind of ugly trick to circumvent the fact that the conda pyqt package is not identified by setuptools nor pkg_resources which can lead to re-installing of failing the checks for requirements. The inconvenience should be minor since exopy cannot start without pyqt5.
    
    * conda: revert stupid change to recipe

--- setup.py
+++ setup.py
@@ -9,6 +9,20 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 from exopy.version import __version__
 
+install_requires = ['atom>=0.4.1', 'enaml>=0.10.2', 'kiwisolver>=1.0.0',
+                    'configobj', 'watchdog', 'qtawesome', 'numpy']
+
+# Avoid adding PyQt5 to install_requires if PyQt5 is already present. This
+# allows to avoid re-installing it if it is already present (typically in a
+# conda environment). This is not perfect but comes from
+# https://github.com/ContinuumIO/anaconda-issues/issues/1554 which prevents
+# setuptools to identify a conda install PyQt5 installation.
+# Based on https://github.com/glue-viz/glue/pull/1836
+try:
+    import PyQt5  # noqa
+except ImportError:
+    install_requires.append('PyQt5')
+
 
 def long_description():
     """Read the project description from the README file.
@@ -43,9 +57,6 @@ setup(
     package_data={'': ['*.enaml', '*.txt']},
     python_requires='>=3.5',
     setup_requires=['setuptools'],
-    install_requires=['atom>=0.4.1', 'enaml>=0.10.2',
-                      'kiwisolver>=1.0.0', 'configobj',
-                      'watchdog', 'qtawesome', 'numpy',
-                      'pyqt5'],
+    install_requires=install_requires,
     entry_points={'gui_scripts': 'exopy = exopy.__main__:main'},
 )
