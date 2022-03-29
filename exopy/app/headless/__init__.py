32e0e8297ffe60c5bfc69f43d6d60b14b1a76a60
Matthieu Dartiailh <m.dartiailh@gmail.com>
2022-03-29 21:21:01 +0200

    add missing __init__.py

---
+++ exopy/app/headless/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2022 by Exopy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""Minimal plugin to allow running exopy in headless mode.
+
+"""
